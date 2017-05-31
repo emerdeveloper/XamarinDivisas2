613b2616bbb16d206498fdf0f1bfe21c700084a1
1388310 <1388310@50HW012242>
2017-05-31 13:27:03 -0500

    Hola, he llegado hasta el paso 9

---
+++ Divisas2/Divisas2/Divisas2/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Divisas2.Views;
+
+using Xamarin.Forms;
+
+namespace Divisas2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            //se borro el MainPage.xaml que se crea en la raiz de la carpeta compartida
+            //new ForeingExchangePage() hace referencia alnombre de la pagina
+            //se borro el MainPage.xaml
+            //Esta en la actividad con la cual se inicia el proyecto
+            MainPage = new NavigationPage(new ForeingExchangePage());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Divisas2/Divisas2/Divisas2/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divisas2.ViewModels
+{
+    public class MainViewModel
+    {
+        public MainViewModel() { }
+
+
+    }
+}
